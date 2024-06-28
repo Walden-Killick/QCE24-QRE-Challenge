8fc9473c2ca835ff5b04e20af8835efa8f69c2ce
Walden-Killick <walden.killick@cambridgeconsultants.com>
2024-06-28 16:31:43 +0100

    feat: create functions for oracles

--- qce24_qre_challenge/quantum_oracles/banded_circulant_oracle.py
+++ qce24_qre_challenge/quantum_oracles/banded_circulant_oracle.py
@@ -3,11 +3,15 @@
 Cambridge Consultants 2024
 Walden Killick
 """
-from quantum_oracles import SparseOracle
-from sparse_matrices import BandedCirculantMatrix
+from qiskit import QuantumCircuit
+from qce24_qre_challenge.quantum_oracles import SparseOracle
+from qce24_qre_challenge.sparse_matrices import BandedCirculantMatrix
 
 
 class BandedCirculantOracle(SparseOracle):
     def __init__(self, banded_circulant_matrix: BandedCirculantMatrix) -> None:
         self._banded_circulant_matrix = banded_circulant_matrix
-    
+    def location_oracle(self) -> QuantumCircuit:
+        return super().location_oracle()
+    def value_oracle(self) -> QuantumCircuit:
+        return super().value_oracle()
