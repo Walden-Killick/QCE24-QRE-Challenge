c54bd5fa34b6323a3cfc1501673d9a0009704c59
Walden-Killick <walden.killick@cambridgeconsultants.com>
2024-07-02 08:51:58 +0100

    feat: create QSVT class and notebook

--- qce24_qre_challenge/circuit/qsvt.py
+++ qce24_qre_challenge/circuit/qsvt.py
@@ -3,11 +3,85 @@
 Cambridge Consultants 2024
 Walden Killick
 """
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit.library.standard_gates import XGate
 from qce24_qre_challenge.circuit import BlockEncoding
 from .circuit import Circuit
 
 
+def signal_processing_circuit(angle: float, num_ancillas: int) -> QuantumCircuit:
+    """Create the signal-processing circuit (projector-controlled rotation).
+
+    Parameters
+    ----------
+    angle : float
+        Phase angle.
+    num_ancillas : int
+        Number of ancillas used in the block encoding.
+
+    Returns
+    -------
+    QuantumCircuit
+        Phase gate controlled by ancillas in the all-zero state. As described in
+        https://doi.org/10.1145/3313276.3316366.
+    """
+    circuit = QuantumCircuit(num_ancillas + 1)
+    n0x_gate = XGate().control(num_ancillas, ctrl_state=0)
+    circuit.append(n0x_gate, reversed(range(num_ancillas+1)))
+    circuit.rz(2*angle, 0)
+    circuit.append(n0x_gate, reversed(range(num_ancillas+1)))
+    return circuit
+
+
 class QSVT(Circuit):
     def __init__(self, block_encoding: BlockEncoding, phase_angles: list[float]) -> None:
+        """Initialise the 'QSVT'.
+
+        Parameters
+        ----------
+        block_encoding : BlockEncoding
+            Block encoding circuit to apply the transformation to.
+        phase_angles : list[float]
+            Phase angles determining the polynomial transformation.
+        """
         self._block_encoding = block_encoding
         self._phase_angles = phase_angles
+    def create_circuit(self) -> QuantumCircuit:
+        """Create the QSVT circuit
+
+        Returns
+        -------
+        QuantumCircuit
+            Circuit which block-encodes a polynomial transformation of the input matrix.
+        """
+        total_qubits = self._block_encoding.num_qubits + 1
+        num_ancillas = self._block_encoding.num_ancillas
+
+        block_encoding_circuit = self._block_encoding.create_circuit()
+        block_encoding_gate = block_encoding_circuit.to_gate()
+        block_encoding_gate.name = 'U_A'
+
+        processing_reg = QuantumRegister(1, name='rot')
+        ancilla_reg = QuantumRegister(num_ancillas, name='anc')
+        output_reg = QuantumRegister(total_qubits-num_ancillas-1, name='out')
+
+        qsvt_circuit = QuantumCircuit(processing_reg, ancilla_reg, output_reg)
+        qsvt_circuit.h(processing_reg)
+
+        parity = 0
+        for angle in self._phase_angles[:-1]:
+            qsvt_circuit.compose(
+                signal_processing_circuit(angle, num_ancillas), inplace=True
+            )
+            if parity == 0:
+                qsvt_circuit.append(block_encoding_gate, range(1, total_qubits))
+                parity = 1
+            else:
+                qsvt_circuit.append(block_encoding_gate.inverse(), range(1, total_qubits))
+                parity = 0
+        qsvt_circuit.compose(
+            signal_processing_circuit(self._phase_angles[-1], num_ancillas), inplace=True
+        )
+        qsvt_circuit.h(0)
+
+        return qsvt_circuit
